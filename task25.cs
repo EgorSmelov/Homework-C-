b379ff6e83f5c11279e5e5610fb025179e75d678
EgorSmelov <smelov909@mail.ru>
2022-12-10 21:07:11 +0300

    готово

---
+++ task25.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введиет число A ");
+int numA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введиет число B ");
+int numB = int.Parse(Console.ReadLine()!);
+
+
+int stepen = Power(numA, numB);
+Console.WriteLine(stepen);
+
+
+int Power(int numberA, int numberB)
+{
+    int riders = numberA;
+    for (int i = 1; i < numberB; i++)
+    {
+        riders = riders * numberA;
+    }
+    return riders;
+}
---
+++ task27.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+
+int function = Power(num);
+Console.WriteLine(function);
+
+int Power(int number)
+{
+    int sum = 0;
+    while (number >= 10)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum + number;
+}
