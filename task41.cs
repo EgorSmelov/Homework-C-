bae5ceaa43a49ca916f3e95bc4946d9d36a7c796
EgorSmelov <smelov909@mail.ru>
2022-12-21 20:01:01 +0300

    готово

---
+++ task41.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите количество чисел");
+int numberCount = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(CountPositive(numberCount));
+
+
+int CountPositive(int numberCount)
+{
+    int counter = 0;
+    for (int i = 0; i < numberCount; i++)
+    {
+        Console.Write($"{i + 1}  ");
+        if (int.Parse(Console.ReadLine()!) > 0) counter++;
+    }
+    return counter;
+}
---
+++ task43.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите b1");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k1");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите b2");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k2");
+double k2 = double.Parse(Console.ReadLine()!);
+
+double[] piece = Function(b1, k1, b2, k2);
+
+Console.WriteLine(piece[0]);
+Console.WriteLine(piece[1]);
+
+double[] Function(double b1, double k1, double b2, double k2)
+{
+    double[] current = new double[2];
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    current[0] = x;
+    current[1] = y;
+    return current;
+}
